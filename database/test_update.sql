c1f85acb8520425bcd356f34ee3c41a5114b33fe
Shihui Lu <shihui.lu51@gmail.com>
2025-12-22 14:42:21 -0500

    Complete update tests

---
+++ database/test_update.sql
@@ -0,0 +1,13 @@
+-- settle_all_owings
+UPDATE Spending
+SET settled = true
+WHERE freebie = false 
+    AND settled = false 
+    AND ARRAY(SELECT UNNEST(spenders) ORDER BY 1) != ARRAY(SELECT UNNEST(beneficiaries) ORDER BY 1);
+
+-- unsettle_all_owings
+UPDATE Spending
+SET settled = false
+WHERE freebie = false 
+    AND settled = true 
+    AND ARRAY(SELECT UNNEST(spenders) ORDER BY 1) != ARRAY(SELECT UNNEST(beneficiaries) ORDER BY 1);
