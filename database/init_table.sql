f6e5f8f2f3ae11a4e0b58fac39f71f3a87975ea0
Shihui Lu <shihui.lu51@gmail.com>
2025-12-30 18:14:58 -0500

    Change naming convention from camelCase to snake_case

--- database/init_table.sql
+++ database/init_table.sql
@@ -1,46 +1,46 @@
-DROP TABLE IF EXISTS SpendingSecondaryCategory;
-DROP TABLE IF EXISTS Spending;
-DROP TABLE IF EXISTS SecondaryCategory;
-DROP TABLE IF EXISTS PrimaryCategory;
-DROP TYPE IF EXISTS Individual;
+DROP TABLE IF EXISTS spending_secondary_category;
+DROP TABLE IF EXISTS spending;
+DROP TABLE IF EXISTS secondary_category;
+DROP TABLE IF EXISTS primary_category;
+DROP TYPE IF EXISTS individual;
 
-CREATE TYPE Individual AS ENUM ('Jaden', 'Jiwon', 'Other');
+CREATE TYPE individual AS ENUM ('Jaden', 'Jiwon', 'Other');
 
-CREATE TABLE PrimaryCategory (
-    primaryCategoryName TEXT PRIMARY KEY
+CREATE TABLE primary_category (
+    primary_category_name TEXT PRIMARY KEY
 );
 
-CREATE TABLE SecondaryCategory (
-    secondaryCategoryName TEXT PRIMARY KEY
+CREATE TABLE secondary_category (
+    secondary_category_name TEXT PRIMARY KEY
 );
 
-CREATE TABLE Spending (
-    spendingId SERIAL PRIMARY KEY,
+CREATE TABLE spending (
+    spending_id SERIAL PRIMARY KEY,
     description TEXT NOT NULL,
     aggregated BOOLEAN NOT NULL,
     cost DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL DEFAULT CURRENT_DATE,
     essential BOOLEAN NOT NULL,
-    spenders Individual[] NOT NULL CHECK(cardinality(spenders) > 0),
-    beneficiaries Individual[] NOT NULL CHECK(cardinality(beneficiaries) > 0),
+    spenders individual[] NOT NULL CHECK(cardinality(spenders) > 0),
+    beneficiaries individual[] NOT NULL CHECK(cardinality(beneficiaries) > 0),
     freebie BOOLEAN NOT NULL,
     settled BOOLEAN NOT NULL DEFAULT false,
-    primaryCategoryName TEXT NOT NULL,
-    FOREIGN KEY (primaryCategoryName) REFERENCES PrimaryCategory(primaryCategoryName)
+    primary_category_name TEXT NOT NULL,
+    FOREIGN KEY (primary_category_name) REFERENCES primary_category(primary_category_name)
         ON DELETE RESTRICT
         ON UPDATE CASCADE,
-    storeName TEXT NOT NULL,
-    storeLocation TEXT NOT NULL
+    store_name TEXT NOT NULL,
+    store_location TEXT NOT NULL
 );
 
-CREATE TABLE SpendingSecondaryCategory (
-    spendingId INTEGER,
-    secondaryCategoryName TEXT,
-    FOREIGN KEY (spendingId) REFERENCES Spending(spendingId)
+CREATE TABLE spending_secondary_category (
+    spending_id INTEGER,
+    secondary_category_name TEXT,
+    FOREIGN KEY (spending_id) REFERENCES spending(spending_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    FOREIGN KEY (secondaryCategoryName) REFERENCES SecondaryCategory(secondaryCategoryName)
+    FOREIGN KEY (secondary_category_name) REFERENCES secondary_category(secondary_category_name)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    PRIMARY KEY (spendingId, secondaryCategoryName)
+    PRIMARY KEY (spending_id, secondary_category_name)
 );
