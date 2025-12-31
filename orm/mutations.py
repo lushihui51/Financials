aee836a2236ed7ab956ad64807d76649402ff7a5
Shihui Lu <shihui.lu51@gmail.com>
2025-12-31 07:59:18 -0500

    Revert "Add default values for settled and aggregated in create_spending"
    This reverts commit 7772afe4249e25c771fb53ca2bb2f9dc511453a6.

--- orm/mutations.py
+++ orm/mutations.py
@@ -4,10 +4,10 @@ from datetime import date
 from db_config import Session
 from models import Spending
 
-def create_spending(description: str, cost: Decimal, date: date, 
+def create_spending(description: str, aggregated: bool, cost: Decimal, date: date, 
                     essential: bool, spenders: List[str], beneficiaries: List[str], 
-                    primary_category_name: str, store_name: str, 
-                    store_location: str, freebie: bool, settled: bool=False, aggregated: bool=True) -> int:
+                    freebie: bool, settled: bool, primaryCategoryName: str, storeName: str, 
+                    storeLocation: str) -> int:
     with Session() as session:
         spending = Spending(
             description=description,
