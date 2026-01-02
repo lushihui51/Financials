3a00fedc8d9c3d49a6bc937552e3149f0ed5355b
Shihui Lu <shihui.lu51@gmail.com>
2026-01-02 15:26:32 -0500

    Change TEXT type to VARCHAR with character limitation

--- database/init_table.sql
+++ database/init_table.sql
@@ -7,16 +7,16 @@ DROP TYPE IF EXISTS individual;
 CREATE TYPE individual AS ENUM ('Jaden', 'Jiwon', 'Other');
 
 CREATE TABLE primary_category (
-    primary_category_name TEXT PRIMARY KEY
+    primary_category_name VARCHAR(50) PRIMARY KEY
 );
 
 CREATE TABLE secondary_category (
-    secondary_category_name TEXT PRIMARY KEY
+    secondary_category_name VARCHAR(50) PRIMARY KEY
 );
 
 CREATE TABLE spending (
     spending_id SERIAL PRIMARY KEY,
-    description TEXT NOT NULL,
+    description VARCHAR(255) NOT NULL,
     aggregated BOOLEAN NOT NULL,
     cost DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL DEFAULT CURRENT_DATE,
@@ -25,17 +25,17 @@ CREATE TABLE spending (
     beneficiaries individual[] NOT NULL CHECK(cardinality(beneficiaries) > 0),
     freebie BOOLEAN NOT NULL,
     settled BOOLEAN NOT NULL DEFAULT false,
-    primary_category_name TEXT NOT NULL,
+    primary_category_name VARCHAR(50) NOT NULL,
     FOREIGN KEY (primary_category_name) REFERENCES primary_category(primary_category_name)
         ON DELETE RESTRICT
         ON UPDATE CASCADE,
-    store_name TEXT NOT NULL,
-    store_location TEXT NOT NULL
+    store_name VARCHAR(50) NOT NULL,
+    store_location VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE spending_secondary_category (
     spending_id INTEGER,
-    secondary_category_name TEXT,
+    secondary_category_name VARCHAR(50),
     FOREIGN KEY (spending_id) REFERENCES spending(spending_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
